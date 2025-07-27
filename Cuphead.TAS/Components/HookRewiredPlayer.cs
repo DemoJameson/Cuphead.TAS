8ea4d2fc5a871a487618dab82ca284504a4eeffb
DemoJameson <demojameson@gmail.com>
2025-07-27 12:08:16 +0800

    fix: confirm button does not work sometimes

--- Cuphead.TAS/Components/HookRewiredPlayer.cs
+++ Cuphead.TAS/Components/HookRewiredPlayer.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using CupheadTAS.Utils;
 using HarmonyLib;
 using Rewired;
@@ -55,13 +55,13 @@ public class TasRewiredPlayer : PluginComponent {
 
     [HarmonyPatch(nameof(Player.GetAxis), typeof(string))]
     [HarmonyPrefix]
-    private static bool GetAxis(string actionName, ref float __result) {
+    private static bool GetAxis(string actionName, ref float __result, Player __instance) {
         if (!Manager.Running || currentInput == null) {
             return true;
         }
 
         if (EnumHelpers<CupheadButton>.TryParse(actionName, out CupheadButton button)) {
-            GetAxis(button, ref __result);
+            GetAxis(button, ref __result, __instance);
         }
 
         return false;
@@ -69,17 +69,22 @@ public class TasRewiredPlayer : PluginComponent {
 
     [HarmonyPatch(nameof(Player.GetAxis), typeof(int))]
     [HarmonyPrefix]
-    private static bool GetAxis(int actionId, ref float __result) {
+    private static bool GetAxis(int actionId, ref float __result, Player __instance) {
         if (!Manager.Running || currentInput == null) {
             return true;
         }
 
         CupheadButton button = (CupheadButton) actionId;
-        GetAxis(button, ref __result);
+        GetAxis(button, ref __result, __instance);
         return false;
     }
 
-    private static void GetAxis(CupheadButton button, ref float __result) {
+    private static void GetAxis(CupheadButton button, ref float __result, Player __instance) {
+        if (IsNotPlayerOne(__instance)) {
+            __result = 0f;
+            return;
+        }
+
         if (button is CupheadButton.MoveHorizontal) {
             if (currentInput.HasActions(Actions.Left)) {
                 __result = -1f;
@@ -97,9 +102,14 @@ public class TasRewiredPlayer : PluginComponent {
 
     [HarmonyPatch(nameof(Player.GetButton), typeof(int))]
     [HarmonyPrefix]
-    private static bool GetButton(int actionId, ref bool __result) {
+    private static bool GetButton(int actionId, ref bool __result, Player __instance) {
         if (!Manager.Running) {
             return true;
+        }
+        
+        if (IsNotPlayerOne(__instance)) {
+            __result = false;
+            return false;
         }
 
         if (TryGetActions(actionId, out var actions)) {
@@ -111,9 +121,14 @@ public class TasRewiredPlayer : PluginComponent {
 
     [HarmonyPatch(nameof(Player.GetButtonDown), typeof(int))]
     [HarmonyPrefix]
-    private static bool GetButtonDown(int actionId, ref bool __result) {
+    private static bool GetButtonDown(int actionId, ref bool __result, Player __instance) {
         if (!Manager.Running) {
             return true;
+        }
+
+        if (IsNotPlayerOne(__instance)) {
+            __result = false;
+            return false;
         }
 
         if (TryGetActions(actionId, out var actions)) {
@@ -125,9 +140,14 @@ public class TasRewiredPlayer : PluginComponent {
 
     [HarmonyPatch(nameof(Player.GetButtonUp), typeof(int))]
     [HarmonyPrefix]
-    private static bool GetButtonUp(int actionId, ref bool __result) {
+    private static bool GetButtonUp(int actionId, ref bool __result, Player __instance) {
         if (!Manager.Running) {
             return true;
+        }
+
+        if (IsNotPlayerOne(__instance)) {
+            __result = false;
+            return false;
         }
 
         if (TryGetActions(actionId, out var actions)) {
@@ -139,9 +159,14 @@ public class TasRewiredPlayer : PluginComponent {
 
     [HarmonyPatch(nameof(Player.GetButtonTimePressed), typeof(int))]
     [HarmonyPrefix]
-    private static bool GetButtonTimePressed(int actionId, ref float __result) {
+    private static bool GetButtonTimePressed(int actionId, ref float __result, Player __instance) {
         if (!Manager.Running) {
             return true;
+        }
+
+        if (IsNotPlayerOne(__instance)) {
+            __result = 0f;
+            return false;
         }
 
         if (TryGetActions(actionId, out var actions) && currentInput?.HasActions(actions) == true) {
@@ -159,6 +184,104 @@ public class TasRewiredPlayer : PluginComponent {
         return false;
     }
 
+    [HarmonyPatch(nameof(Player.GetAnyButton))]
+    [HarmonyPrefix]
+    private static bool GetAnyButton(Player __instance, ref bool __result) {
+        if (!Manager.Running) {
+            return true;
+        }
+
+        __result = false;
+
+        CupheadButton[] buttons = (CupheadButton[]) Enum.GetValues(typeof(CupheadButton));
+        foreach (CupheadButton button in buttons) {
+            if (__instance.GetButton((int) button)) {
+                __result = true;
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    [HarmonyPatch(nameof(Player.GetAnyButtonDown))]
+    [HarmonyPrefix]
+    private static bool GetAnyButtonDown(Player __instance, ref bool __result) {
+        if (!Manager.Running) {
+            return true;
+        }
+
+        __result = false;
+
+        CupheadButton[] buttons = (CupheadButton[]) Enum.GetValues(typeof(CupheadButton));
+        foreach (CupheadButton button in buttons) {
+            if (__instance.GetButtonDown((int) button)) {
+                __result = true;
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    [HarmonyPatch(nameof(Player.GetAnyButtonUp))]
+    [HarmonyPrefix]
+    private static bool GetAnyButtonUp(Player __instance, ref bool __result) {
+        if (!Manager.Running) {
+            return true;
+        }
+
+        __result = false;
+
+        CupheadButton[] buttons = (CupheadButton[]) Enum.GetValues(typeof(CupheadButton));
+        foreach (CupheadButton button in buttons) {
+            if (__instance.GetButtonUp((int) button)) {
+                __result = true;
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    [HarmonyPatch(typeof(CupheadInput.AnyPlayerInput), nameof(CupheadInput.AnyPlayerInput.GetAnyButtonDown))]
+    [HarmonyPrefix]
+    private static bool AnyPlayerInputGetAnyButtonDown(CupheadInput.AnyPlayerInput __instance, ref bool __result) {
+        if (!Manager.Running) {
+            return true;
+        }
+
+        CupheadButton[] buttons = (CupheadButton[]) Enum.GetValues(typeof(CupheadButton));
+        foreach (CupheadButton button in buttons) {
+            if (__instance.GetButtonDown(button)) {
+                __result = true;
+                return false;
+            }
+        }
+
+        return false;
+    }
+    
+    [HarmonyPatch(typeof(CupheadInput.AnyPlayerInput), nameof(CupheadInput.AnyPlayerInput.GetAnyButtonHeld))]
+    [HarmonyPrefix]
+    private static bool AnyPlayerInputGetAnyButtonHold(CupheadInput.AnyPlayerInput __instance, ref bool __result) {
+        if (!Manager.Running) {
+            return true;
+        }
+
+        __result = false;
+
+        CupheadButton[] buttons = (CupheadButton[]) Enum.GetValues(typeof(CupheadButton));
+        foreach (CupheadButton button in buttons) {
+            if (__instance.GetButton(button)) {
+                __result = true;
+                return false;
+            }
+        }
+
+        return false;
+    }
+
     [HarmonyPatch(typeof(InputManager_Base), "Update")]
     [HarmonyPrefix]
     private static bool InputManagerUpdate() {
@@ -170,4 +293,6 @@ public class TasRewiredPlayer : PluginComponent {
     private static bool InputManagerFixedUpdate() {
         return !Manager.Running;
     }
+    
+    private static bool IsNotPlayerOne(Player player) => player != PlayerManager.GetPlayerInput(PlayerId.PlayerOne);
 }
